0b253595a098a510219aa468922e65ceecee5b9c
MuKuL-DiXiT <mukuldixit086@gmail.com>
2025-05-01 01:00:19 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,41 +3,41 @@ import { NavLink, Link } from "react-router-dom";
 import { Home, User, Code, Folder, Contact, Sun, Moon, Menu } from "lucide-react";
 
 export default function Navbar({ darkMode, setDarkMode }) {
-
+    const navClass  = ({ isActive }) => `flex items-center ml-3 gap-4 ${isActive && 'text-red-600'}`;
     return (
         <div className={`mt-5 border-b-2 flex-wrap ${darkMode ? 'text-orange-200 border-b-orange-200' : 'text-black border-b-black'} flex justify-evenly rounded-full `}>
             <div className="font-marker text-3xl text-red-600">
                 MD
             </div>
-            <div className="flex flex-wrap justify-center sm:gap-10 md:gap-8 lg:gap-5 items-baseline">
+            <div className="flex flex-wrap justify-center mt-1 sm:gap-10 md:gap-8 lg:gap-5 items-baseline">
                 <div>
-                    <NavLink to="/" className={({ isActive }) => `flex items-center ml-3 gap-4 ${isActive && 'text-red-600'}`}>
+                    <NavLink to="/" className={navClass}>
                         <Home />
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to="/Skills" className={({ isActive }) => `flex items-center ml-3 gap-4 ${isActive && 'text-red-600'}`}>
+                    <NavLink to="/Skills" className={navClass}>
                         <Code />
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to="/Projects" className={({ isActive }) => `flex items-center ml-3 gap-4 ${isActive && 'text-red-600'}`}>
+                    <NavLink to="/Projects" className={navClass}>
                         <Folder />
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to="/Contacts" className={({ isActive }) => `flex items-center ml-3 gap-4 ${isActive && 'text-red-600'}`}>
+                    <NavLink to="/Contacts" className={navClass}>
                         <Contact />
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to="/About" className={({ isActive }) => `flex items-center ml-3 gap-4 ${isActive && 'text-red-600'}`}>
+                    <NavLink to="/About" className={navClass}>
                         <User />
                     </NavLink>
                 </div>
             </div>
             <div>
-                <button onClick={() => setDarkMode(!darkMode)} className="mb-6 rounded-full ">
+                <button onClick={() => setDarkMode(!darkMode)} className="mb-6 mt-1 rounded-full duration-1000 transition-colors">
                     {darkMode ? <Sun/> : <Moon/>}
                 </button>
             </div>
