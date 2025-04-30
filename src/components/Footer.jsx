6a8020a3877c4decb1fc09b9c274cb6c22b3727e
MuKuL-DiXiT <mukuldixit086@gmail.com>
2025-05-01 00:59:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,19 @@ import { div } from 'framer-motion/client';
 
 export default function Footer({ darkMode }) {
     return (
-        <div className={`mt-5 border-t-2 text-2xl flex flex-col items-center flex-wrap ${darkMode ? 'text-orange-200 border-b-orange-200' : 'text-black border-t-black'}  justify-evenly rounded-full `}>
+        <div className={` mt-5 border-t-2 lg:text-2xl md:text-2xl sm:text-xl flex flex-col items-center flex-wrap ${darkMode ? 'text-orange-200 border-b-orange-200' : 'text-black border-t-black'}  justify-evenly rounded-full `}>
             <div className='flex gap-5'>
                 <a href="https://github.com/MuKuL-DiXiT" target='_blank'><FaGithub /></a>
                 <a href="https://www.linkedin.com/in/mukul-dixit-8b945227b/" target='_blank'><FaLinkedin /></a>
                 <a href="https://leetcode.com/mukul2427/" target="_blank" rel="noopener noreferrer">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/LeetCode_logo_white_no_text.svg"
-                        alt="LeetCode Profile" className="w-4 bg-black rounded" />
+                        alt="LeetCode Profile" className="w-5 bg-black rounded" />
                 </a>
                 <a href='https://t.me/mukuldixit' target='_blank'><FaTelegram /></a>
                 <a href="https://www.instagram.com/mukul____dixit/" target='_blank'><FaInstagram /></a>
             </div>
             <div className='flex gap-2'>
-                Created By <span className="text-red-600 font-marker">
+                Created By <span className="text-red-600">
         <Typewriter
           options={{
             strings: ["Mukul Dixit"],
